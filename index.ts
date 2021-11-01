f7c383b02d5d35b49e02b0a0506dde06e393c5b9
Nang Chan <nangchan@gmail.com>
2021-10-31 19:53:17 -0700

    update message

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ import {
 } from 'https://deno.land/std@0.111.0/http/server.ts';
 
 function handler(_request: Request, _connInfo: ConnInfo) {
-  return new Response('Hello world');
+  return new Response('Hello World!');
 }
 
 console.log('Listening on http://localhost:8000');
