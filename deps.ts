2f325e14e7ace53dee0d134c7ece9cba23a0841c
Nang Chan <nangchan@gmail.com>
2021-11-01 09:24:19 -0700

    Using manual file serving since automatic static file serving crashes deno deploy

--- deps.ts
+++ deps.ts
@@ -5,9 +5,14 @@ export {
   HttpError,
   Status
 } from 'https://deno.land/x/oak@v9.0.1/mod.ts';
-export type { ServerRequest, ServerResponse } from 'https://deno.land/x/oak@v9.0.1/mod.ts';
-export type { RouteParams } from 'https://deno.land/x/oak@v9.0.1/mod.ts';
+export type {
+  ServerRequest,
+  ServerResponse,
+  RouteParams
+} from 'https://deno.land/x/oak@v9.0.1/mod.ts';
+
 export { listenAndServe } from 'https://deno.land/std@0.111.0/http/server.ts';
 export type { ConnInfo } from 'https://deno.land/std@0.111.0/http/server.ts';
+
 export { extname } from 'https://deno.land/std@0.99.0/path/posix.ts';
 export { bold, cyan, green, red, yellow } from 'https://deno.land/std@0.107.0/fmt/colors.ts';
--- serve.ts
+++ serve.ts
@@ -4,71 +4,84 @@
  * @see https://github.com/oakserver/oak/blob/main/examples/staticServer.ts
  */
 
-import { Application, HttpError, Status, bold, cyan, green, red, yellow } from './deps.ts';
+/** oak/mod.ts */
+import { Application, Router, Context } from './deps.ts';
+/** oak/mod.ts */
+import type { RouteParams } from './deps.ts';
+/** path/posix.ts */
+import { extname } from './deps.ts';
+/** fmt/colors.ts */
+import { bold, yellow } from './deps.ts';
 
-const app = new Application();
+/** Based on file_server.ts */
+const MEDIA_TYPES: Record<string, string> = {
+  '.md': 'text/markdown',
+  '.html': 'text/html',
+  '.htm': 'text/html',
+  '.json': 'application/json',
+  '.map': 'application/json',
+  '.txt': 'text/plain',
+  '.ts': 'text/typescript',
+  '.tsx': 'text/tsx',
+  '.js': 'application/javascript',
+  '.jsx': 'text/jsx',
+  '.gz': 'application/gzip',
+  '.css': 'text/css',
+  '.wasm': 'application/wasm',
+  '.mjs': 'application/javascript',
+  '.svg': 'image/svg+xml',
+  '.png': 'image/png',
+  '.bmp': 'image/bmp',
+  '.jpg': 'image/jpg',
+  '.webp': 'image/webp',
+  '.woff': 'font/woff',
+  '.woff2': 'font/woff2'
+};
 
-// Error handler middleware
-app.use(async (context, next) => {
-  try {
-    await next();
-  } catch (e) {
-    if (e instanceof HttpError) {
-      // deno-lint-ignore no-explicit-any
-      context.response.status = e.status as any;
-      if (e.expose) {
-        context.response.body = `<!DOCTYPE html>
-            <html>
-              <body>
-                <h1>${e.status} - ${e.message}</h1>
-              </body>
-            </html>`;
-      } else {
-        context.response.body = `<!DOCTYPE html>
-            <html>
-              <body>
-                <h1>${e.status} - ${Status[e.status]}</h1>
-              </body>
-            </html>`;
-      }
-    } else if (e instanceof Error) {
-      context.response.status = 500;
-      context.response.body = `<!DOCTYPE html>
-            <html>
-              <body>
-                <h1>500 - Internal Server Error</h1>
-              </body>
-            </html>`;
-      console.log('Unhandled Error:', red(bold(e.message)));
-      console.log(e.stack);
+/** Returns the content-type based on the extension of a path. */
+function contentType(path: string): string | undefined {
+  return MEDIA_TYPES[extname(path)];
+}
+
+/**
+ * Serve all files from directory (without slash at end) recursively
+ * @example serveFilesFrom('./public')
+ * @param directory Pathname without slash (/) as end
+ */
+async function serveFilesFrom(
+  router: Router<RouteParams, Record<string, string>>,
+  directory: string,
+  original = directory
+) {
+  for await (const dirEntry of Deno.readDir(directory)) {
+    if (dirEntry.isDirectory) {
+      // recursively add filepath to route
+      serveFilesFrom(router, directory + '/' + dirEntry.name, original);
+    } else {
+      // add filepath to route
+      const urlpath =
+        '/' +
+        (directory + '/').replace(original + '/', '') +
+        (dirEntry.name === 'index.html' ? '' : dirEntry.name);
+      router.get(urlpath, async (context: Context) => {
+        const filepath = directory + '/' + dirEntry.name;
+        // get content from defined mime-types
+        const type = contentType(filepath);
+        context.response.type = type;
+        // move file open inside of closure to
+        // control lifetime of file handler
+        const body = await Deno.readFile(filepath); // open in binary in case images
+        context.response.body = body;
+      });
     }
   }
-});
+}
 
-// Logger
-app.use(async (context, next) => {
-  await next();
-  const rt = context.response.headers.get('X-Response-Time');
-  console.log(
-    `${green(context.request.method)} ${cyan(context.request.url.pathname)} - ${bold(String(rt))}`
-  );
-});
-
-// Response Time
-app.use(async (context, next) => {
-  const start = Date.now();
-  await next();
-  const ms = Date.now() - start;
-  context.response.headers.set('X-Response-Time', `${ms}ms`);
-});
-
-// Send static content
-app.use(async (context) => {
-  await context.send({
-    root: 'build',
-    index: 'index.html'
-  });
-});
+const router = new Router();
+serveFilesFrom(router, 'build');
+const app = new Application();
+app.use(router.routes());
+app.use(router.allowedMethods());
 
 app.addEventListener('listen', ({ hostname, port, serverType }) => {
   console.log(bold('Start listening on ') + yellow(`${hostname}:${port}`));
