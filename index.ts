a714f3fb8353166db994058baadaad6caed54d73
Nang Chan <nangchan@gmail.com>
2021-10-31 19:30:03 -0700

    Initial commit

---
+++ index.ts
@@ -0,0 +1,11 @@
+import {
+  listenAndServe,
+  ConnInfo,
+} from 'https://deno.land/std@0.111.0/http/server.ts';
+
+function handler(_request: Request, _connInfo: ConnInfo) {
+  return new Response('Hello world');
+}
+
+console.log('Listening on http://localhost:8000');
+await listenAndServe(':8000', handler);
